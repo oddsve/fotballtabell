e9310ec86bd82f666df723f8316796ab7e5d0268
Odd Morten Sveås <odd.morten.sveas@acando.no>
2018-03-11 21:15:04 +0100

    dummy

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
